b0a7db24890cffecbe7dcb28de90ecf778c2a3f3
Son MP <thaison1962003@gmail.com>
2026-01-16 04:31:26 +0700

    update

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,22 +1,51 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize('quanlylichkham', 'root', null, {
-    host: 'localhost',
-    dialect: 'mysql',
-    timezone: "+07:00",
-    dialectOptions: {
-        dateStrings: true,
-        typeCast: true,
-        timezone: "+07:00"
-    },
-    logging: false
-})
+const env = process.env.NODE_ENV || 'development';
+
+let sequelize;
+
+if (env === 'production') {
+    sequelize = new Sequelize(
+        process.env.DB_DATABASE_NAME,
+        process.env.DB_USERNAME,
+        process.env.DB_PASSWORD,
+        {
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT || 3306,
+            dialect: 'mysql',
+            logging: false,
+            query: { raw: true },
+            timezone: "+07:00",
+            dialectOptions: {
+                ssl: {
+                    require: true,
+                    rejectUnauthorized: false
+                },
+                dateStrings: true,
+                typeCast: true,
+                timezone: "+07:00"
+            }
+        }
+    );
+} else {
+    sequelize = new Sequelize('quanlylichkham', 'root', null, {
+        host: 'localhost',
+        dialect: 'mysql',
+        timezone: "+07:00",
+        dialectOptions: {
+            dateStrings: true,
+            typeCast: true,
+            timezone: "+07:00"
+        },
+        logging: false
+    });
+}
 
 let connectDB = async () => {
     try {
         await sequelize.authenticate();
-        await sequelize.query("SET GLOBAL max_allowed_packet=67108864");
-        console.log('Connection DB has been established successfully.');
+        console.log(`Connection DB (${env}) has been established successfully.`);
     } catch (error) {
         console.error('Unable to connect to the database:', error);
     }
