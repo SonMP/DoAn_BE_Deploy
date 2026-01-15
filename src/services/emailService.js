384535b636ed0168efa36d1c2828eb0b9d5e92e7
Son MP <thaison1962003@gmail.com>
2026-01-16 04:26:13 +0700

    update eamil

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -3,13 +3,19 @@ require('dotenv').config();
 
 let createTransporter = () => {
     return nodemailer.createTransport({
-        service: "Gmail",
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
         auth: {
             user: process.env.EMAIL_APP,
             pass: process.env.EMAIL_APP_PASSWORD,
         },
-        logger: true, // Log to console
-        debug: true, // Include debug info
+        tls: {
+            rejectUnauthorized: false
+        },
+        family: 4,
+        logger: true,
+        debug: true,
     });
 }
 
